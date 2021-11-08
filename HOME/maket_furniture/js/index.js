b02d24fa7c7baa1e1b709749526a25880f820a2e
DARIYA SNITICH <sinklap@gmail.com>
2021-11-08 23:07:29 +0300

    done maket

--- HOME/maket_furniture/js/index.js
+++ HOME/maket_furniture/js/index.js
@@ -1,34 +1,50 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 // log
-$(function() {
-	$(".btn").click(function() {
-		$(".form-signin").toggleClass("form-signin-left");
+$(function () {
+  $(".btn").click(function () {
+    $(".form-signin").toggleClass("form-signin-left");
     $(".form-signup").toggleClass("form-signup-left");
     $(".frame").toggleClass("frame-long");
     $(".signup-inactive").toggleClass("signup-active");
     $(".signin-active").toggleClass("signin-inactive");
-    $(".forgot").toggleClass("forgot-left");   
+    $(".forgot").toggleClass("forgot-left");
     $(this).removeClass("idle").addClass("active");
-	});
+  });
 });
 
-$(function() {
-	$(".btn-signup").click(function() {
-  $(".nav").toggleClass("nav-up");
-  $(".form-signup-left").toggleClass("form-signup-down");
-  $(".success").toggleClass("success-left"); 
-  $(".frame").toggleClass("frame-short");
-	});
+$(function () {
+  $(".btn-signup").click(function () {
+    $(".nav").toggleClass("nav-up");
+    $(".form-signup-left").toggleClass("form-signup-down");
+    $(".success").toggleClass("success-left");
+    $(".frame").toggleClass("frame-short");
+  });
 });
 
-$(function() {
-	$(".btn-signin").click(function() {
-  $(".btn-animate").toggleClass("btn-animate-grow");
-  $(".welcome").toggleClass("welcome-left");
-  $(".cover-photo").toggleClass("cover-photo-down");
-  $(".frame").toggleClass("frame-short");
-  $(".profile-photo").toggleClass("profile-photo-down");
-  $(".btn-goback").toggleClass("btn-goback-up");
-  $(".forgot").toggleClass("forgot-fade");
-	});
+$(function () {
+  $(".btn-signin").click(function () {
+    $(".btn-animate").toggleClass("btn-animate-grow");
+    $(".welcome").toggleClass("welcome-left");
+    $(".cover-photo").toggleClass("cover-photo-down");
+    $(".frame").toggleClass("frame-short");
+    $(".profile-photo").toggleClass("profile-photo-down");
+    $(".btn-goback").toggleClass("btn-goback-up");
+    $(".forgot").toggleClass("forgot-fade");
+  });
 });
