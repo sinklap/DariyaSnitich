edcbb6ac3b0a8383c72a5c21273e7badbb2ddb53
DARIYA SNITICH <sinklap@gmail.com>
2022-01-30 19:52:08 +0300

    done maket

---
+++ python/first_app/firstpage/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта'})
+
+
+def about(request):
+    return render(request, 'main/about.html')
